b02a9e9c49f580f37b31b9555cd02f11f17f0097
Łukasz Wroński <lukaszwronski98@gmail.com>
2021-08-25 11:57:40 +0200

    Use debian and centos image to test packages

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -4,8 +4,8 @@ set -eu
 docker build -t scala-cli-alpine -f .github/scripts/docker/AlpineDockerFile . 
 docker run scala-cli-alpine about
 
-docker build -t scala-cli-ubuntu -f .github/scripts/docker/UbuntuDockerFile . 
+docker build -t scala-cli-ubuntu -f .github/scripts/docker/DebianDockerFile .
 docker run scala-cli-ubuntu about
 
-docker build -t scala-cli-fedora -f .github/scripts/docker/FedoraDockerFile . 
+docker build -t scala-cli-fedora -f .github/scripts/docker/CentOSDockerFile .
 docker run scala-cli-fedora about
